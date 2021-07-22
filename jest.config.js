29141a24eae57e536a1ca063a910e7d969cf64b1
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-07-22 15:08:19 +0200

    nits

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  roots: ['<rootDir>/packages'],
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/__tests__/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
---
+++ packages/branch-name/src/main.ts
@@ -0,0 +1,41 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+function generateDockerTag(name: string): string {
+
+  if (name === 'refs/heads/master') {
+    return 'latest'
+  }
+
+  return name.replace(/\//g, '_')
+}
+
+function getBranchName(name: string | undefined) {
+
+  if (!name) {
+    throw new Error('name cannot be undefined')
+  }
+
+  return name.replace('refs/heads/', '')
+}
+
+async function main() {
+
+  let branchName: string
+  let dockerTag: string
+
+  switch(github.context.eventName) {
+    case 'pull_request':
+      branchName = getBranchName(process.env.GITHUB_HEAD_REF)
+      dockerTag = generateDockerTag(branchName)
+      break
+    default:
+      branchName = getBranchName(process.env.GITHUB_REF)
+      dockerTag = generateDockerTag(branchName)
+  }
+
+  core.setOutput('currentBranch', branchName)
+  core.setOutput('dockerTag', dockerTag)
+}
+
+main()
