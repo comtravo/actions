780786efdf8b1b58c18332b26a9b0f769261e5bb
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-07-23 16:53:32 +0200

    add more stuff

--- packages/archive-artifacts-s3/src/main.ts
+++ packages/archive-artifacts-s3/src/main.ts
@@ -1,1 +1,35 @@
-import * as AWS from 'aws-sdk'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+async function main(): Promise<void> {
+  try {
+    const artifactPath = core.getInput('artifactPath', {
+      required: true
+    })
+    const bucketName = core.getInput('bucketName', {
+      required: true
+    })
+    const prefix = core.getInput('prefix', {
+      required: true
+    })
+    const generatePresignedURL =
+      core.getInput('generatePresignedURL') === 'true'
+    const presignedURLTTLSeconds = core.getInput('presignedURLTTLSeconds')
+    const s3Link = `s3://${bucketName}/${prefix}`
+    await exec.exec(
+      `aws s3 sync ${artifactPath} ${s3Link} --acl bucket-owner-full-control`
+    )
+
+    core.setOutput('s3Link', s3Link)
+
+    if (generatePresignedURL) {
+      await exec.exec(
+        `aws s3 presign ${s3Link} --expires-in ${presignedURLTTLSeconds}`
+      )
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
