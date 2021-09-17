945307fef653ed935fb6d0da3fe8dbc1aec9745c
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-09-17 13:38:15 +0200

    Add validate-inputs action

---
+++ packages/validate-inputs/src/main.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+
+async function main(): Promise<void> {
+  try {
+    let input = core.getInput('input', {
+      required: true
+    })
+
+    input = input.toLowerCase()
+
+    const blacklistCSV = core.getInput('blacklist', {
+      required: true
+    })
+
+    let blacklist = blacklistCSV.split(',')
+    blacklist = blacklist.map(b => b.toLowerCase())
+
+    core.info(`Input to validate: ${input}`)
+    core.info(`Input to validate against ${blacklist}`)
+
+    if (blacklist.includes(input)) {
+      throw new Error(`Input: ${input} part of blacklist: ${blacklist}`)
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
